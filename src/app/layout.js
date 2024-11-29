8d0ed9cf457efca956ef6e2af12b88fbe4de11b1
VishalSh20 <140361454+VishalSh20@users.noreply.github.com>
2024-11-29 13:24:44 +0530

    Added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "@/components/Footer";
-
+import {Analytics} from "@vercel/analytics/next"
 import { Poppins } from "next/font/google";
 
 const geistSans = localFont({
@@ -33,6 +33,7 @@ export default function RootLayout({ children }) {
         <ClerkProvider>
         <Header/>
         {children}
+        <Analytics/>
         <Footer/>
         </ClerkProvider>
       </body>
